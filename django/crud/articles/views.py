ba840b8411408ff996676c273b85059219eed803
hyooo1017 <hyooo1017@gmail.com>
2019-10-30 14:05:30 +0900

    191030 django db 연결 crud 프로젝트

--- django/crud/articles/views.py
+++ django/crud/articles/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
 
 # Create your views here.
 def index(request):
-    articles = Article.objects.all()    # => Query Set ~= List
+    articles = Article.objects.all().order_by('-id')    # => Query Set ~= List
 
     context = {
         'articles' : articles,
@@ -16,14 +16,52 @@ def new(request):
 
 def create(request):
     # 넘어온 데이터 받기
-    title = request.GET.get('title')
-    content = request.GET.get('content')
+    # 이유 2번 - GET(URL), POST(HTTP body)
+    title = request.POST.get('title')
+    content = request.POST.get('content')
 
     article = Article.objects.create(title = title, content = content)
     # article.title이나 article.content로 확인 가능
+    return redirect(f'/articles/{article.pk}/')
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+       
+    context = {
+        'article' : article,
+    }
+    return render(request, 'articles/detail.html', context)
+
+def delete(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.delete()
+    context = {
+        'article' : article,
+    }
+    return redirect('/articles/')
+
+def edit(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article' : article,
+    }
+    return render(request, 'articles/edit.html', context)
+
+def update(request, pk):
+    # 1. pk에 해당하는 article 가져오기
+    article = Article.objects.get(pk=pk)
+
+    # 2. edit로부터 넘어온 데이터 가져오기
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+    
+    # 3. 넘어온 데이터를 article에 새롭게 저장
+    article.title = title
+    article.content = content
+    article.save()
 
     context = {
-        'title' : title,
-        'content' : content,
+        'article' : article,
     }
-    return render(request, 'articles/create.html', context)
+    return redirect(f'/articles/{article.pk}/')     # detail 페이지로 redirect
+
