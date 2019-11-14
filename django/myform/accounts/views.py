3698c8fe7e7725eeb8a25b7a3e97c78440e31cfb
hyooo1017 <hyooo1017@gmail.com>
2019-11-14 13:49:30 +0900

    191114 django 비밀번호 변경, 로그인 관련 기능 추가

--- django/myform/accounts/views.py
+++ django/myform/accounts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth import login as auth_login     # 밑에 정의된 login과 구별하기 위해
 from django.contrib.auth import logout as auth_logout
 from .forms import CustomUserChangeForm
+from django.contrib.auth import update_session_auth_hash
 
 # Create your views here.
 def signup(request):
@@ -47,7 +48,9 @@ def login(request):
             # 3. 로그인 수행
             auth_login(request, form.get_user())
             # 4. redirect -> 메인 페이지 (articles index)
-            return redirect('articles:index')
+            # 4-1. next 값 가져오기
+            next = request.GET.get('next')
+            return redirect(next or 'articles:index')       # next값이 있다면 로그인 후 next 주소로 돌아감
     else:
         # 로그인 창 보여줌
         form = AuthenticationForm()
@@ -56,6 +59,23 @@ def login(request):
         'form' : form,
     }
     return render(request, 'accounts/login.html', context)
+
+# Signup vs. Login
+# 1. Signup
+# User를 Create
+
+# 2. Login
+# Session을 Create
+
+# 3. Logout
+# Session을 Delete
+
+
+# Session이란?
+# Django가 브라우저의 정보를 가져와
+# 임시로 들고 있도록 해서
+# 지금 이 페이지를 보는 User가 누구인지
+# 서버쪽에서 정보를 들고 있는 것.
 
 # POST 요청만 받음
 def logout(request):
@@ -92,21 +112,24 @@ def delete(request):
         request.user.delete()
         return redirect('articles:index')
 
-# Signup vs. Login
-# 1. Signup
-# User를 Create
+def password(request):
+    if request.method == 'POST':
+        # 실제로 비밀번호 변경
+        # 1. 넘어온 데이터 Form에 입력
+        form = PasswordChangeForm(request.user, request.POST)
+        # 2. 검증
+        if form.is_valid():
+            # 3. 비밀번호 저장
+            user = form.save()
+            # 3-1. 세션 유지 (로그인 유지)
+            update_session_auth_hash(request, user)
+            # 4. redirect -> 메인 페이지
+            return redirect('articles:index')
+    else:
+        # 비밀번호를 변경하는 양식 보여줌
+        form = PasswordChangeForm(request.user)
 
-# 2. Login
-# Session을 Create
-
-# 3. Logout
-# Session을 Delete
-
-
-
-# Session이란?
-# Django가 브라우저의 정보를 가져와
-# 임시로 들고 있도록 해서
-# 지금 이 페이지를 보는 User가 누구인지
-# 서버쪽에서 정보를 들고 있는 것.
-
+    context = {
+        'form' : form,
+    }
+    return render(request, 'accounts/password.html', context)
--- django/myform/articles/views.py
+++ django/myform/articles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Article
 from .forms import ArticleForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
@@ -10,30 +11,11 @@ def index(request):
     }
     return render(request, 'articles/index.html', context)
 
-# def new(request):
-#     if request.method == 'POST':
-#         # 데이터베이스에 데이터 생성 (Form)
-#         # 1. 넘어온 데이터를 받기
-#         article_form = ArticleForm(request.POST)
-#         # title = request.POST.get('title')
-#         # content = request.POST.get('content')
-#         # 2. 넘어온 데이터 검증
-#         if article_form.is_valid():
-#             title = article_form.cleaned_data.get('title')
-#             content = article_form.cleaned_data.get('content')
-#             # 3. 데이터베이스에 Article 만들기!
-#             article = Article.objects.create(title=title, content=content)
-#             # 4. redirect -> detail
-#             return redirect('articles:detail', article.pk)
-#     else:
-#         article_form = ArticleForm()
-#         # 폼을 보여줌
-#         context = {
-#             'article_form': article_form,
-#         }
-#         return render(request, 'articles/new.html', context)
 
-
+# new함수에 접근할 때 먼저 로그인 되어 있는지 확인
+# 만일 로그인되어 있지 않다면 로그인 페이지로 자동으로 넘어감
+# 그 때 주소에서 next값으로 로그인 확인을 요구했던 페이지를 값으로 갖고 있음
+@login_required
 def new(request):
     if request.method == 'POST':
         # 데이터베이스에 데이터 생성 (ModelForm)
@@ -56,6 +38,7 @@ def new(request):
     }
     return render(request, 'articles/new.html', context)
 
+@login_required
 def detail(request, pk):
     # 1. pk번째 데이터를 가져오기
     article = Article.objects.get(pk=pk)
@@ -66,34 +49,7 @@ def detail(request, pk):
     # 3. render와 함께 html로 넘겨주기
     return render(request, 'articles/detail.html', context)
 
-
-# def edit(request, pk):
-#     article = Article.objects.get(pk=pk)
-#     if request.method == 'POST':
-#         # Article 수정
-#         # 1. 넘어온 데이터 받기
-#         article_form = ArticleForm(request.POST)
-#         # 2. 데이터 검증
-#         if article_form.is_valid():
-#             # 3. 검증된 데이터로 수정 & 저장
-#             article.title = article_form.cleaned_data.get('title')
-#             article.content = article_form.cleaned_data.get('content')
-#             article.save()
-#             # 4. redirect -> detail
-#             return redirect('articles:detail', article.pk)
-#     else:
-#         # Article 수정하는 Form 보여주기
-#         article_form = ArticleForm(
-#                 {
-#                     'title': article.title,
-#                     'content': article.content,
-#                 }
-#             )
-#         context = {
-#             'article_form': article_form,
-#         }
-#         return render(request, 'articles/new.html', context)
-
+@login_required
 def edit(request, pk):
     article = Article.objects.get(pk=pk)
     if request.method == 'POST':
